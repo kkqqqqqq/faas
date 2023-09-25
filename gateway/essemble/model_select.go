201a4de8847e59b9d6013a86d682e25c4f54365a
孔可青 <1804626972@qq.com>
2023-09-25 17:26:05 +0800

    9.25

--- gateway/essemble/model_select.go
+++ gateway/essemble/model_select.go
@@ -1,20 +1,11 @@
-package  essemble
-
-import(
-
-
-
-	//"fmt"
-
-)
-
+package essemble
 
 // '''
 // Terms:
 
-//  latency:	max latency amont all instances, 
-//  accuracy:	aggreagted aacuracy of all instances, 
-// 	total cost of all instances 
+//  latency:	max latency amont all instances,
+//  accuracy:	aggreagted aacuracy of all instances,
+// 	total cost of all instances
 
 // slo_latency, slo_accuracy, slo_cost : constraints provided by user. (FIXME: Need some defaut values for each)
 
@@ -28,14 +19,14 @@ import(
 // 		instance.add_model_to_instance	:	keeps track of all models in an instace and adds models as scale-in is called #can be called instance_model_manager
 // 											returns 1 in case of successful scale in, returns 0 if fail
 // 		scale_up						: 	adds one more instace to the instance list
-		
+
 // 	TODO:
 // 		scale_down_policy				: incase the user changes the SLO in the runtime
 // '''
 
 // input: latency,acc, of user
 
-var model_list =[]string{
+var model_list = []string{
 	"nasnetlarge",
 	"inceptionresnetv2",
 	"xception",
@@ -48,6 +39,16 @@ var model_list =[]string{
 	"mobilenetv2",
 	"vgg16",
 	"mobilenet"}
+var models map[string]model
+
+type model struct {
+	name      string
+	latency   float32
+	accuracy  float32
+	coldstart float32
+	memory    int
+	cpu       float32
+}
 
 //var model_latency = [315,151.96, 119.2, 74, 152.21, 89.5, 102.35, 98.22, 78.18, 41.5, 259, 43.45]
 
@@ -55,58 +56,70 @@ var model_list =[]string{
 
 //var model_coldstart =[]
 
-func  ModelSelection(slo_latency float32 ,slo_accuracy float32, mode string) (model_selected []string){
-	
-	if mode == "cocktail"{
+func ModelSelection(slo_latency float32, slo_accuracy float32, mode string) (model_selected []string) {
+
+	if mode == "cocktail" {
 		// cocketail
 	}
-	
-	if mode=="efaas"{
+
+	if mode == "inference" {
+		//使用满足acc的单个模型
+	}
+
+	if mode == "efaas" {
 		//efaas
 		//input: latency ,acc
 		//factors: latency,acc,cpu,memory,cold/warm
-		// target_latency :=latency
-		// target_acc := accuracy
-		// model_selected :=[]string
-		
-		essemble_latency = 0
-		essemble_accuracy = 0
 
-		for essemble_accuracy<slo_accuracy && essemble_latency>slo_latency{
+		modelInit()
+
+		essemble_latency := float32(0)
+		essemble_accuracy := float32(0)
+
+		for essemble_accuracy < slo_accuracy && essemble_latency > slo_latency {
 
 		}
 
+		model_selected = []string{"mobilenet", "mobilenetv2", "resnet50v2", "nasnetmobile"}
 
-
-		model_selected = []string{"mobilenet","mobilenetv2","resnet50v2","nasnetmobile"}
-		
 	}
 	return
 
 }
 
-func getEssembleLatency(model_selected []string)(essembleLatency float32){
+func getEssembleLatency(model_selected []string) (essembleLatency float32) {
 	essembleLatency = 0
-	for model := range model_selected {
-		essembleLatency = max(essembleLatency,getSingleLatency(model))
+	for _, model := range model_selected {
+		essembleLatency = max(essembleLatency, getSingleLatency(model))
 	}
 	return essembleLatency
 }
 
-func getSingleLatency(model string)(latency float32){
-	latency := getModelLatency(model)
-	if  coldStart(model)
-		latency = getColdStartLatency(model)+latency
+func getSingleLatency(model string) (latency float32) {
+	latency = getModelLatency(model)
+	if coldStart(model) {
+		latency = getColdStartLatency(model) + latency
+	}
 	return latency
 }
 
-func coldStart(model string)(bool){
+func coldStart(model string) bool {
 	return false
 }
 
-func getColdStartLatency(model string)(latency float32){
-	return 11
+func getColdStartLatency(model string) (latency float32) {
+	return models[model].coldstart
 }
-func getModelLatency(model string)(latency float32){
-	return 11
+func getModelLatency(model string) (latency float32) {
+	return models[model].latency
+}
+
+func max(a float32, b float32) float32 {
+	if a > b {
+		return a
+	}
+	return b
+}
+func modelInit() {
+	models["nast"] = model{"nasnet", 1.0, 1.0, 22.0, 100, 0.2}
 }
